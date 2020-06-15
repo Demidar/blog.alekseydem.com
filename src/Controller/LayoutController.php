c162ae54a0994cdd4968fd7c9e338e346c61fb58
Dem <aleksey.dem6@gmail.com>
2020-06-15 16:13:04 +0500

    initial setup; first controllers; login workflow.

---
+++ src/Controller/LayoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LayoutController extends AbstractController
+{
+    public function renderHeaderNavigation()
+    {
+
+    }
+}
