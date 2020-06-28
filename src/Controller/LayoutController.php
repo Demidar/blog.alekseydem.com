75952316324bc2ef7289d18699a7f172cde8c295
Dem <aleksey.dem6@gmail.com>
2020-06-28 16:07:16 +0500

    first scratch of layout and header

--- src/Controller/LayoutController.php
+++ src/Controller/LayoutController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class LayoutController extends AbstractController
 {
-    public function renderHeaderNav()
+    public function renderHeaderNav(): Response
     {
         return $this->render('layout/_header-nav.html.twig');
     }
